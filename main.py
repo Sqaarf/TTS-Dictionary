d1fd4aea6c17d939d334dc800dd6becc0b38e7af
MrScarf <mrscarf19@gmail.com>
2021-08-13 20:42:30 +0200

    Added input

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from tts import TextToSpeech as TTS
 from dictio import Dictio
 
 if __name__ == '__main__':
-
-	defi = " ".join(Dictio().scrape('chair'))
+	word = input("Word you want the definition : ")
+	defi = " ".join(Dictio().scrape(word))
 	print(defi)
 	TTS().run(defi)
 	
